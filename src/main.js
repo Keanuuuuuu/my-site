3fa6507a09c2788cb26b14e0f72d9da8b1ef6000
Keanuuuuuu <93373675+Keanuuuuuu@users.noreply.github.com>
2023-08-16 16:51:42 +0800

    ✨ feat(消息弹窗组件): 1、封装消息弹窗组件，挂载到Vue.prototype身上 2、封装request请求，定义mock数据模拟响应

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
+import "./mock";
+// 在mock的index中专门导入数据
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import "./style/global.less"
-
+import showMessage from "./utils/showMessage";
+Vue.prototype.$showMessage = showMessage;
 Vue.config.productionTip = false
 
 new Vue({
